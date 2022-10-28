36a82f36191fa15559c73115c9995833ed4f20e8
martinrybka2000 <rybatinek14@gmail.com>
2022-10-28 12:51:50 +0200

    Added ProgramData structure
    Also added the VESC library becouse of the need to add Vesc.data to the structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
 
+#include "ProgramData.h"
+
+// main program data
+ProgramData programData;
+
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("Hello world");
+  programData.BatteryTemperature = 22.4;
 }
 
 void loop()
 {
-  Serial.println("Hello world");
+  Serial.printf("prgoramData.BatterTem = %f\n", programData.BatteryTemperature);
   delay(1000);
 }
