b8773405a66644614409600ac36ef634dc433aac
martinrybka2000 <rybatinek14@gmail.com>
2022-10-28 13:03:49 +0200

    Added DataSmoothing class
    It uses a cpp Queue library https://github.com/SMFSW/Queue to stare the data and calculates the averege value

---
+++ src/DataSmoother.cpp
@@ -0,0 +1,42 @@
+
+#include "DataSmoother.h"
+
+DataSmoother::DataSmoother(size_t N)
+{
+    DataQueue = new cppQueue(sizeof(double), N, FIFO, true);
+}
+
+DataSmoother::~DataSmoother()
+{
+    delete DataQueue;
+}
+
+void DataSmoother::AddData(double data)
+{
+    DataQueue->push(&data);
+}
+
+double DataSmoother::GetData()
+{
+    double average = 0;
+
+    for (size_t i = 0; i < DataQueue->getCount(); i++)
+    {
+        double tem = 0;
+        DataQueue->peekIdx(&tem, i);
+        average += tem;
+    }
+
+    return average / DataQueue->getCount();
+}
+
+void DataSmoother::PrintData()
+{
+    double res;
+    for (size_t i = 0; i < DataQueue->getCount(); i++)
+    {
+        DataQueue->peekIdx(&res, i);
+        Serial.printf("%d: ", i);
+        Serial.println(res);
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include <Arduino.h>
 
 #include "ProgramData.h"
+#include "DataSmoother.h"
 
 // main program data
 ProgramData programData;
+DataSmoother dataSmoother(10);
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Hello world");
   programData.BatteryTemperature = 22.4;
+
+  // data smoother test
+  for (size_t i = 0; i < 20; i++)
+  {
+    dataSmoother.AddData(i);
+  }
+  dataSmoother.PrintData();
 }
 
 void loop()
 {
   Serial.printf("prgoramData.BatterTem = %f\n", programData.BatteryTemperature);
+  Serial.printf("DataSmoother result = %f\n", dataSmoother.GetData());
   delay(1000);
 }
